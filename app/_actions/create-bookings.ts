086dabe93c880108a2c7e2e458e0962ab7a66701
MiguelASFerreira <migant1306@gmail.com>
2024-08-11 12:22:13 -0300

    chore: update auth setup

--- app/_actions/create-bookings.ts
+++ app/_actions/create-bookings.ts
@@ -2,16 +2,21 @@
 
 import { revalidatePath } from "next/cache"
 import { db } from "../_lib/prisma"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
 
 interface CreateBookingsParams {
-  userId: string
   serviceId: string
   date: Date
 }
 
 export const CreateBookings = async (params: CreateBookingsParams) => {
+  const user = await getServerSession(authOptions)
+
+  if (!user) throw new Error("Usuário não autentificado")
+
   await db.booking.create({
-    data: params,
+    data: { ...params, userId: (user.user as any).id },
   })
 
   revalidatePath("/barbershops/[id]")
